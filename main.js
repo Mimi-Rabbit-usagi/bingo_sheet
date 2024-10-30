6357557b59e067569bb5726a265f14ce527b7bd6
RabbitMimi <kitto.ringo@gmail.com>
2024-10-30 22:38:11 +0900

    コードをまとめる

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
 "use strict";
 {
   function createColumn(col) {
-    //const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     const source = [];
     for (let i = 0; i < 15; i++) {
       source[i] = i + 1 + 15 * col;
     }
 
     const column = [];
-    //   b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-    //   b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-    //   b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-    //   b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-    //   b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-
     for (let i = 0; i < 5; i++) {
       column[i] = source.splice(
         Math.floor(Math.random() * source.length),
@@ -24,20 +17,27 @@
     return column;
   }
 
-  const columns = [];
-  columns[0] = createColumn(0);
-  columns[1] = createColumn(1);
-  columns[2] = createColumn(2);
-  columns[3] = createColumn(3);
-  columns[4] = createColumn(4);
-  columns[2][2] = "FREE";
+  function createColumns() {
+    const columns = [];
+    for (let i = 0; i < 5; i++) {
+      columns[i] = createColumn(i);
+    }
+    columns[2][2] = "FREE";
+    return columns;
+  }
 
-  const bingo = [];
-  for (let row = 0; row < 5; row++) {
-    bingo[row] = [];
-    for (let col = 0; col < 5; col++) {
-      bingo[row][col] = columns[col][row];
+  function renderBingo(columns) {
+    for (let row = 0; row < 5; row++) {
+      const tr = document.createElement("tr");
+      for (let col = 0; col < 5; col++) {
+        const td = document.createElement("td");
+        td.textContent = columns[col][row];
+        tr.appendChild(td);
+      }
+      document.querySelector("tbody").appendChild(tr);
     }
   }
-  console.table(columns);
+
+  const columns = createColumns();
+  renderBingo(columns);
 }
